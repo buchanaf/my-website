5d0cb43cb844eaf158466a88ced1ca753c67baf8
buchanaf <buchanaf@gmail.com>
2016-01-21 18:47:11 -0500

    google analytics fix

--- src/index.js
+++ src/index.js
@@ -3,12 +3,19 @@ import { render } from 'react-dom';
 import { Router } from 'react-router';
 import createBrowserHistory from 'history/lib/createBrowserHistory';
 import routes from './routes';
+import GA from 'react-ga';
 
 require('css/main.css');
 
+function stateUpdate() {
+  window.scrollTo(0, 0);
+  GA.pageview(window.location.pathname);
+}
+
 window.onload = function initApp() {
+  GA.initialize('UA-72758609-1');
   render((
-    <Router history={createBrowserHistory()} onUpdate={() => window.scrollTo(0, 0)}>
+    <Router history={createBrowserHistory()} onUpdate={stateUpdate}>
       {routes}
     </Router>
   ), document.getElementById('app'));
