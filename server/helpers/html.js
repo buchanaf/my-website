acb800b863553e3823148cbb088b2a68592f5e42
buchanaf <buchanaf@gmail.com>
2016-03-01 21:56:30 -0500

    try external css file again

--- server/helpers/html.js
+++ server/helpers/html.js
@@ -2,7 +2,6 @@ import React, {Component, PropTypes} from 'react';
 import { renderToString } from 'react-dom/server';
 import Helmet from 'react-helmet';
 import serialize from 'serialize-javascript';
-import css from '../../src/css/main.css';
 
 export default class html extends Component {
   static propTypes = {
@@ -29,7 +28,6 @@ export default class html extends Component {
                   rel="stylesheet" type="text/css"/>)}
 
           {/* resolves the initial style flash (flicker) on page load in development mode */}
-          { <style dangerouslySetInnerHTML={{__html: css }}/>}
 
         </head>
 
